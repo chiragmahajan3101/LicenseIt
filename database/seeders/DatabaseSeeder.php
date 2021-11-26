05f326dbb0f13d0ce7ddaba1736514227c623dee
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-11-26 16:31:55 +0530

    updated user attributes and default users are made using seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,24 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // Admin User
+        User::create([
+            'name' =>     "Admin LicenseIt",
+            'email' => "adminlicenseit@gmail.com",
+            'email_verified_at' => now(),
+            'password' => Hash::make('Licenseit@123'),
+            'role' => 1,
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' =>     "Chirag Mahajan",
+            'email' => "chiragmahajan3101@gmail.com",
+            'email_verified_at' => now(),
+            'password' => Hash::make('chirag3101'),
+            'role' => 0,
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
