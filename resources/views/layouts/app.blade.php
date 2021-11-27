c64246f98bc5d82b2f2c63741d7ebe5534c0710d
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-11-27 12:51:57 +0530

    scripts for smotth scroll and typing efect

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,9 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@600;800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@800&display=swap" rel="stylesheet">
     <link rel="icon" href="{{ asset('images/favico.ico') }}">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -27,14 +28,53 @@
 <body>
     <div id="app">
         @include('layouts.partials._navbar')
-
         <main class="py-4">
             @yield('content')
+            <a  href="#app"
+                title="GO TO TOP"
+                class="btn btn-orange mb-5 mr-3 rounded-circle animate__animated  animate__bounceInDown animate__slow "
+                id="back-to-top">
+                <i class="fa fa-arrow-up"></i>
+            </a>
         </main>
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+     <!-- JQUERY VALIDATOR-->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js" integrity="sha512-37T7leoNS06R80c8Ulq7cdCDU5MNQBwlYoy1TX/WUsLFC2eYNqtKlV0QjH7r8JpG/S0GUMZwebnVFLPd6SU5yg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="{{ asset('js/MySmoothScroll.js') }}"></script>
+    <script src="{{ asset('js/RevealOnScroll.js') }}"></script>
+    <script src="{{asset('js/Typing.js')}}"></script>
+    <script>
+        $('#back-to-top').hide();
+        new MySmoothScroll("#back-to-top");
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 60) {
+                $('#back-to-top').fadeIn('2000');
+                $('.navbar').addClass('shadow-sm');
+                $('.navbar').removeClass('bg-white');
+                $('.navbar').addClass('bg-light');
+                $('#back-to-top').removeClass('animate__bounceOutUp');
+            } else {
+                $('#back-to-top').addClass('animate__bounceOutUp');
+                $('.navbar').removeClass('shadow-sm');
+                $('.navbar').addClass('bg-white');
+                $('.navbar').removeClass('bg-light');
+            }
+        });
+        var typed = new Typed('.intro-text', {
+        strings: ["<h1>First sentence.</h1>", "<h1>Log in</h1>", "<h1>Manage License</h1>"],
+        typeSpeed: 80,
+        startDelay: 1,
+        backSpeed: 40,
+        loop: true,
+        showCursor: false
+        });
+    </script>
 
     @yield('scripts')
 </body>
