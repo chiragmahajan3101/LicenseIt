9e182ef5b333f0843e7fbf51b157e91e5d7272c0
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-11-27 12:51:37 +0530

    updated and completed login UI with all functionalities

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,71 +3,70 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+        <div class="col-md-12">
+            <div class="container-login-left bg-secondary"></div>
+            <div class="container-login-left"></div>
+            <div class="container-login d-flex flex-row justify-content-between reveal animate__pulse animate__animated">
+                <div class="display-image animate__fadeIn animate__animated animate__slow">
+                    <img src="images/login/login.png" class="image-display" width="450em" alt="">
+                </div>
+                <div class="animate__fadeIn animate__animated animate__slow flex-column">
+                    <div class="intro-text">
+                    </div>
+                    <div class="inputs">
+                        <form method="POST" action="{{ route('login') }}">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                            <div class="email-input form-group-row">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" class="@error('email') is-invalid @enderror" id="email" placeholder="Enter your email address"  value="{{ old('email') }}">
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                            <div class="password-input  form-group-row">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" id="password" class="@error('password') is-invalid @enderror" placeholder="Enter your Password">
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                            <div class="form-group row">
+                                <div class="col-md-7">
+                                    <div class="form-check pt-4">
+                                        <input class="form-check-input pt-5" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        <label class="form-check-label" for="remember" style="padding: 0;">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                </div>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
+                            <button type="submit" class="d-block mx-auto styled-btn styled-rounded text-muted float-left border border-dark btn-box" >
+                                <span class="styled-button-text" id="my-btn">Log in</span>
+                            </button>
+                            <div class="form-group-row mt-5">
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
+            <div class="container-login-right bg-secondary"></div>
+            <div class="container-login-right"></div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+<script>
+    new RevealScroll($(".reveal"), "60%");
+</script>
 @endsection
