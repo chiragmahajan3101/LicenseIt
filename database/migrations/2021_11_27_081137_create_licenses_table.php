a700f2df2d1de522f2d41f2147fc9e0e407c8c30
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-11-27 14:15:48 +0530

    migration for software prodcyt and license and its respective attributes

---
+++ database/migrations/2021_11_27_081137_create_licenses_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLicensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('licenses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('buyer_id');
+            $table->unsignedBigInteger('software_id');
+            $table->timestamp('buy_date')->nullable();
+            $table->timestamp('activation_date')->nullable();
+            $table->timestamp('expiry_date')->nullable();
+            $table->unsignedInteger('amount');
+            $table->boolean('active_status')->default(false);
+            $table->string('activation_code');
+            $table->string('transaction_id');
+            $table->text('notes');
+            $table->timestamps();
+
+            $table->foreign('buyer_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->foreign('software_id')->references('id')->on('software')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('licenses');
+    }
+}
