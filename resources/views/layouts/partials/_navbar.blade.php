5fc6fe666d65c6d73d513bda26f22dbff4e047c6
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-11-27 12:59:15 +0530

    updated navbar links and logo for the panel

--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light sticky-top bg-white">
     <div class="container">
         <a class="navbar-brand text-hblack" href="{{ url('/') }}" style="font-family: 'Rubik', sans-serif;">
-            <img src="images/logo2.svg" width="70em"> LicenseIt
+            <img src="images/logo2.svg" width="70em"> <span class="text-orange">L</span>icense<span class="text-orange">I</span>t
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
@@ -15,8 +15,9 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
-                <a href="" class="nav-link text-muted font-weight-bold">Courses</a>
-                <a href="" class="nav-link text-muted font-weight-bold">Quiz</a>
+                @auth
+                <a href="" class="nav-link text-muted font-weight-bold">Dashboard</a>
+                @endauth
                 @guest
                     @if (Route::has('login'))
                         <li class="nav-item">
@@ -26,12 +27,13 @@
 
                     @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="btn btn-orange" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            {{-- <a class="btn btn-orange" href="{{ route('register') }}">{{ __('Register') }}</a> --}}
+                            <a class="btn btn-orange" href="">Welcome</a>
                         </li>
                     @endif
                 @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle text-hblack font-weight-bold" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle text-white font-weight-bold btn btn-hblack" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
 
