c7ab69c2b5a439fa893640740d20b781f1f5c4cc
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-11-29 21:43:56 +0530

    changes in seeder to seed dummy data for license

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Software;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use DateTime;
 
 class DatabaseSeeder extends Seeder
 {
@@ -38,5 +40,27 @@ class DatabaseSeeder extends Seeder
             'mobile_no' => 9067893300,
             'remember_token' => Str::random(10),
         ]);
+
+        Software::factory(10)->create();
+
+        $GLOBALS['buyDate'] = [new DateTime('2021-09-20'), new DateTime('2021-11-25'), new DateTime('2020-01-10'), new DateTime('2021-01-03'), new DateTime('2021-05-17')];
+        // $GLOBALS['activationdate'] = [new DateTime('2021-09-25'), new DateTime('2021-11-25'), new DateTime('2020-01-15'), new DateTime('2021-01-04'), new DateTime('2021-05-20')];
+        // $GLOBALS['expiryDate'] = [new DateTime('2022-09-25'), new DateTime('2022-05-25'), new DateTime('2020-09-15'), new DateTime('2021-11-04'), new DateTime('2021-10-20')];
+
+        User::factory(8)->create()->each(function (User $user) {
+            for ($i = 0; $i < rand(2, 4); $i++) {
+                $j = rand(0, 4);
+                $user->licenses()->create([
+                    'software_id' => rand(1, 10),
+                    'buy_date' => $GLOBALS['buyDate'][$j],
+                    'amount' => rand(350, 1500),
+                    'active_status' => random_int(0, 1),
+                    'activation_code' => Str::random(6),
+                    'hardware_id' => Str::random(9),
+                    'transaction_id' => Str::random(10),
+                    'notes' => Str::random(4)
+                ]);
+            }
+        });
     }
 }
