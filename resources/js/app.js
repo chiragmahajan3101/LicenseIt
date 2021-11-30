4d02203ac6100487d061ac889badfa4c238e14aa
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-11-30 13:29:57 +0530

    installed charts.js for visualization through graphs

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,4 @@ require('./bootstrap');
 require('waypoints/lib/jquery.waypoints');
 require('jquery-smooth-scroll/jquery.smooth-scroll');
 require('typed.js/lib/typed');
+require('chart.js/dist/chart');
