980b8b85751113c23b7f6182ada3d25c31157710
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-12-03 00:05:25 +0530

    introduced scope for email search and action buttons getters also boot methods for auto incrementing new company ID for users

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,6 +41,33 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    // Boot Methods
+    public static function boot()
+    {
+        parent::boot();
+        static::created(function (User $user) {
+            if ($user->role == User::USER_ADMIN) {
+                $user->update([
+                    'company_id' => ""
+                ]);
+            } else {
+                $lastUser = User::orderBy('id', 'DESC')->skip(1)->first();
+                if (strcmp($lastUser->company_id, "") == 0) {
+                    $user->update([
+                        'company_id' => "ML_1"
+                    ]);
+                } else {
+                    $lastId = $lastUser->company_id;
+                    $arr = explode("_", $lastId);
+                    $newId = (int)$arr[1] + 1;
+                    $user->update([
+                        'company_id' => "ML_" . (string)$newId
+                    ]);
+                }
+            }
+        });
+    }
+
     // HELPER FUNCTIONS
     public function isAdmin()
     {
@@ -50,6 +77,24 @@ class User extends Authenticatable
     public function isBuyer()
     {
         return $this->role === self::USER_BUYER;
+    }
+
+    //SCOPES
+    public function scopeSearch($query)
+    {
+        $email = request('email');
+        if ($email) {
+            $user = User::where('email', $email)->first();
+            $id = $user->id;
+            return $query->where("id", $id);
+        }
+        return $query;
+    }
+
+    // Getters
+    public function getActionButtonsAttribute()
+    {
+        return ("<a href='/users/$this->id/edit' class='btn btn-sm btn-info' title='Edit User'>Edit <i class='fa fa-edit fa-lg'></i></a>");
     }
 
     // RELATIONSHIPS
