1e1296fa2853c432518638e6463e49e9b3438727
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-12-03 00:06:15 +0530

    routes to manage users list

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\LicenseController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,3 +34,10 @@ Route::get('/billing', [LicenseController::class, 'bills'])->middleware(['auth']
 
 // License
 Route::resource('/licenses', LicenseController::class);
+
+// User
+Route::get('/users', [UserController::class, 'index'])->middleware(['auth'])->name('users.index');
+Route::post('/users', [UserController::class, 'store'])->middleware(['auth'])->name('users.store');
+Route::get('/users/create', [UserController::class, 'create'])->middleware(['auth'])->name('users.create');
+Route::put('/users/{user}', [UserController::class, 'update'])->middleware(['auth'])->name('users.update');
+Route::get('/users/{user}/edit', [UserController::class, 'edit'])->middleware(['auth'])->name('users.edit');
